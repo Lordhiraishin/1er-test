10348cd9877f2a956b571c07ea6a0163f629add1
lordhiraishin <lemardeley.quentin.pro@hotmail.fr>
2021-10-05 16:24:04 +0200

    1

---
+++ test/Projet 1/ListeListe.py
@@ -0,0 +1,89 @@
+[
+    [MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM]
+    [MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM]
+    [MMMMMMJJJJJJJJJJJJJEEEEEJXJJJJJJJJJJJJJJJJJJMMMXM]
+    [MMMMMJJJJJJJJJJJJJJJJEEEEEEJJJJJJJJJJJJJJJJJJJJJM
+    [MMMMJJJJJJJJJJJJJJJJEEEEEJJJJJJJJJJJJJJJJJJJJJJJM
+    [MMMJJJJJJJJJJJJJJJJJJEEEEEJJJJJJJJJJJJJJJJJJJJMMM
+    [MMMJJJJJJJJJJJJJJJJJJJEEEEEEJJJJJJJJJJJJJJJMMMMMM
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+    [
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# One = "M"*50
+# Three = ("M"*6) + ("J"*13)+("E"*5)+("J")+("X")+("J"*18)+("M"*4)+"X"+"M"
+# Four = ("M"*5)+("J"*16)+("E"*5)+("J"*23)+"M"
+# Five = ("M"*4)+("J"*17)+("E"*5)+("J"*23)+"M"
+# Six = ("M"*3)+("J"*19)+("E"*5)+("J"*20)+"M"*3
+# print(One)
+# print(Three)
+# print(Four)
+# print(Five)
+# print(Six)
+
+
+
+
+
+
+
+
+
+
+
+# Liste1 = [One]
+# Liste2 = [One]
+# Liste3 = [Three]
---
+++ test/Projet 1/Main.py
@@ -0,0 +1,24 @@
+# coding utf-8
+
+# Imports
+
+# Variables
+
+# Functions
+
+
+def main():
+    pass 
+
+
+
+def start():
+
+    PlayerName = input("Quel est votre nom ?")
+
+
+
+
+# Code starts here
+if __name__ == "__main__":
+    Main()
